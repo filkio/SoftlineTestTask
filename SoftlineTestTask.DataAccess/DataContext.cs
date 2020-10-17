4a46b8def9509078e486da37f210e9a5ff6a8812
filkio <72137117+filkio@users.noreply.github.com>
2020-10-17 11:20:03 +0300

    Добавьте файлы проекта.

---
+++ SoftlineTestTask.DataAccess/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftlineTestTask.DataAccess.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftlineTestTask.DataAccess
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public DataContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EmployeesDb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ SoftlineTestTask.DataAccess/Entity/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftlineTestTask.DataAccess.Entity
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }       
+        public string Position { get; set; }        
+        public DateTime DateOfBirth { get; set; }      
+        public string Text { get; set; }
+        public bool Decree { get; set; }
+    }
+}
